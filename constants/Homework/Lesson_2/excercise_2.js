87f5a2851cd55ac6c3dad0846324c805ee78235f
HeliosAiden <heliosaidenismyname@gmail.com>
2023-12-05 13:30:31 +0700

    homework _> home

--- constants/Homework/Lesson_2/excercise_2.js
+++ constants/Homework/Lesson_2/excercise_2.js
@@ -1,12 +1,12 @@
 export const MainElements = [
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_2/bai1.html',
+        src: 'home/Lesson_2/excercise_2/bai1.html',
         style: 'width: 80%; height: 80vh; margin-left:20%;'
     },
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_2/bai2.html',
+        src: 'home/Lesson_2/excercise_2/bai2.html',
         style: 'width: 80%; height: 80vh; margin-left:20%; margin-top: 50px'
     }
 ]
--- constants/Homework/Lesson_2/excercise_3.js
+++ constants/Homework/Lesson_2/excercise_3.js
@@ -1,22 +1,22 @@
 export const MainElements = [
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_3/bai1.html',
+        src: 'home/Lesson_2/excercise_3/bai1.html',
         style: 'width: 80%; height: 25vh; margin-left:20%;'
     },
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_3/bai2.html',
+        src: 'home/Lesson_2/excercise_3/bai2.html',
         style: 'width: 80%; height: 50vh; margin-left:20%; margin-top: 50px'
     },
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_3/bai3.html',
+        src: 'home/Lesson_2/excercise_3/bai3.html',
         style: 'width: 80%; height: 40vh; margin-left:20%; margin-top: 50px'
     },
     {
         tag: 'iframe',
-        src: 'homework/Lesson_2/excercise_3/bai4.html',
+        src: 'home/Lesson_2/excercise_3/bai4.html',
         style: 'width: 80%; height: 50vh; margin-left:20%; margin-top: 50px'
     },
 ]
